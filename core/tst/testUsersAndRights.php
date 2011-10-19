57279d281fc03e03292fac105eb9c10e18f8da29
Grindhold <grindhold@gmx.net>
2011-10-19 23:02:15 +0200

    finalized testAuth

--- core/tst/testUsersAndRights.php
+++ core/tst/testUsersAndRights.php
@@ -43,7 +43,7 @@ class ArrayTest extends PHPUnit_Framework_TestCase {
 		
 		$this->assertFalse($user->authenticate("tochangepassword"));
 		$user->alterPassword("tochangepassword", "testpassword");
-		$this->assertTrue($user->authenticate("tochangepassword"));
+		$this->assertTrue( $user->authenticate("tochangepassword"));
 		$user->alterPassword("testpassword", "tochangepassword");
 		$this->assertFalse($user->authenticate("tochangepassword"));
 		
