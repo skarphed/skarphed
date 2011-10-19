9f5f7a17d09f4559a5a55bc63ce78e42183a442d
Grindhold <grindhold@gmx.net>
2011-10-19 22:40:14 +0200

    Fehler im passworttest behoben

--- core/tst/testUsersAndRights.php
+++ core/tst/testUsersAndRights.php
@@ -39,7 +39,7 @@ class ArrayTest extends PHPUnit_Framework_TestCase {
 	public function testAlterPassword(){
 		$userM = $this->fixture->getUserManager();
 		$user = $userM->getUserByName("genericTestUser");
-		$this->assertFalse($user->authenticate("tochangepassword"));
+		$this->assertTrue($user->authenticate("testpassword"));
 		$user->alterPassword("tochangepassword", "testpassword");
 		$this->assertTrue($user->authenticate("tochangepassword"));
 		$user->alterPassword("testpassword", "tochangepassword");
