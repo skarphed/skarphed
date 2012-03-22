84d3627664821b7cb1d25629cf183605f110f45d
Grindhold <grindhold@gmx.net>
2012-03-22 15:58:32 +0100

    getMethoden des ActionManagers fertiggestellt. Noetige getter und setter in Menu, MenuItem, Action und ActionList gebaut

--- core/lib/action.php
+++ core/lib/action.php
@@ -44,9 +44,9 @@
 		protected function init(){}
 
 		private $currentParent = null;
-
-		public function createAction(){
-			//TODO: Implement
+		
+		public function createAction($siteId, $url, $widgetId, $spaceId){
+			
 		}
 		
 		public function createActionList(){
@@ -61,22 +61,79 @@
 			//TODO: Implement
 		}
 		
-		
-		
 		public function getActionById($actionId){
-			//TODO: Implement	
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$res = $db->query($core, "SELECT ACT_NAME, ACT_ATL_ID, ACT_SIT_ID, 
+												ACT_SPACE, ACT_WGT_ID, ACT_URL, ACT_ORDER
+											 FROM ACTIONS WHERE ACT_ID = ?;", array($actionId));
+			if ($set = $db->fetchObject($res)) {
+				$action = new Action();
+				if (isset($set->ACT_SIT_ID)){
+					$action->setSiteId($set->ACT_SIT_ID);
+				}
+				if (isset($set->ACT_URL)){
+					$action->setUrl($set->ACT_URL);
+				}
+				if (isset($set->ACT_WGT_ID) and isset($set->ACT_SPACE)){
+					$action->setWidgetSpaceConstellation($set->ACT_WGT_ID, $set->ACT_SPACE);
+				}
+				$action->setId($actionId);
+				$action->setName($set->ACT_NAME);
+				$action->setActionListId($set->ACT_ATL_ID);
+				$action->setOrder($set->ACT_ORDER);
+				
+				return $action;
+			}
+			return null;	
 		}	
 		
 		public function getActionListById($actionListId){
-			//TODO: Implement
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$res = $db->query($core, "SELECT ATL_NAME FROM ACTIONLISTS WHERE ATL_ID = ?;", array($actionListId));
+			if ($set = $db->fetchObject($res)){
+				$actionList = new ActionList();
+				$actionList->setId($actionListId);
+				$actionList->setName($set->ATL_NAME);
+				return $actionList;	
+			}
+			return null;
 		}
 		
 		public function getMenuItemById($menuItemId){
-			//TODO: Implement
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$res = $db->query($core, "SELECT MNI_NAME, MNI_MNU_ID, MNI_MNI_ID, MNI_ATL_ID, MNI_ORDER WHERE MNI_ID = ?;",array($menuItemId));
+			if ($set = $db->fetchObject($res)){
+				$menuItem = new MenuItem();
+				$menuItem->setId($menuItemId);
+				$menuItem->setName($set->MNI_NAME);
+				$menuItem->setOrder($set->MNI_ORDER);
+				if (isset($set->MNI_MNU_ID)){
+					$menuItem->setMenuId($set->MNI_MNU_ID);
+				}
+				if (isset($set->MNI_MNI_ID)){
+					$menuItem->setParentMenuItemId($set->MNI_MNI_ID);
+				}
+				if (isset($set->MNI_ATL_ID)){
+					$menuItem->setActionListId($set->MNI_ATL_ID);
+				}
+			}				
+			return null;
 		}
 		
 		public function getMenuById($menuId){
-			//TODO: Implement
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$res = $db->query($core,"SELECT MNU_NAME FROM MENU WHERE MNU_ID = ? ;",array($menuId));
+			if ($set = $db->fetchObject($res)){
+				$menu = new Menu();
+				$menu->setId($menuId);
+				$menu->setName($set->MNU_NAME);
+				return $menu;
+			}
+			return null;
 		}
 	}
 	
@@ -95,6 +152,42 @@
 			
 			$this->siteId = null;
 			$this->url = null;
+			
+			$this->name = null;
+			$this->order = null;
+			$this->actionListId = null;
+			$this->order = null;
+		}
+		
+		public function setOrder($orderNumber){
+			$this->order = $orderNumber;
+		}
+		
+		public function getOrder(){
+			return $this->order;
+		}
+		
+		public function setActionListId($actionListId){
+			$this->actionListId =$actionListId;
+		}
+		
+		public function setName($name){
+			$this->name = $name;
+		}
+		
+		public function getName(){
+			return $this->name;
+		}
+		
+		public function getActionList(){
+			$core = Core::getInstance();
+			$am = $core->getActionManager();
+			$actionList = $am->getActionListById($this->getActionListId()); 
+			return $actionList;
+		}
+		
+		public function getActionListId(){
+			return $this->actionListId;
 		}
 		
 		public function setId($id){
@@ -140,7 +233,10 @@
 		}
 		
 		public function unsetLinks(){
-			//TODO: Implement
+			$this->siteId = null;
+			$this->widgetId = null;
+			$this->spaceId = null;
+			$this->url = null;
 		}
 
 	}
@@ -155,10 +251,28 @@
 		function __const(){
 			//TODO: Implement
 			$this->children = array();
+			$this->id = null;
+			$this->name = null;
 		}
 		
 		public function delete(){
 			//TODO: Implement
+		}
+		
+		public function setName($name){
+			$this->name = $name;
+		}
+		
+		public function getName(){
+			return $this->name;
+		}
+		
+		public function setId($id){
+			$this->id = $id;
+		}
+		
+		public function getId(){
+			return $this->id;
 		}
 		
 		public function addAction($action){
@@ -201,7 +315,15 @@
 	 */
 	class MenuItem  {
 		function __const(){
-			//TODO: Implement
+			$this->id = null;
+			$this->name =null;
+			$this->actionListId = null;
+			$this->actionList = null;
+			$this->menuId = null;
+			$this->menu = null;
+			$this->parentMenuItemId = null;
+			$this->parentMenuItem = null;
+			$this->order = null;
 		}
 		
 		public function render(){
@@ -212,7 +334,83 @@
 			//TODO: Implement
 		}
 		
-		public function setActionList($actionList){
+		public function setOrder($order){
+			$this->order = (int)$order;
+		}
+		
+		public function getOrder(){
+			return $this->order;
+		}
+		
+		public function setActionListId($actionListId){
+			$this->actionListId = (int)$actionListId;
+		}
+		
+		public function getActionList(){
+			
+			if ($this->actionList == null or $this->actionList->getId() != $this->actionListId){
+				$core = Core::getInstance();
+				$am = $core->getActionManager();
+				$this->actionList = $am->getActionListById($this->getActionListId());
+			}
+			return $this->actionList;
+		}
+		
+		public function getActionListId(){
+			return $this->actionListId;
+		}
+		
+		public function setMenuId($menuId){
+			$this->menuId = $menuId;
+		}
+		
+		public function getMenu(){
+			if ($this->menu == null or $this->menu->getId() != $this->menuId){
+				$core = Core::getInstance();
+				$am = $core->getActionManager();
+				$this->menu = $am->getMenuById($this->getMenuId());
+			}
+			return $this->menu;
+		}
+		
+		public function getMenuId(){
+			return $this->menuId;
+		}
+		
+		public function setParentMenuItemId($parentMenuItemId){
+			$this->parentMenuItemId = $parentMenuItemId;
+		}
+		
+		public function getParentMenuItem(){
+			if ($this->parentMenuItem == null or $this->parentMenuItem->getId() != $this->parentMenuItemId){
+				$core = Core::getInstance();
+				$am = $core->getActionManager();
+				$this->parentMenuItem = $am->getMenuItemById($this->getParentMenuItemId());
+			}
+			return $this->parentMenuItem;
+		}
+		
+		public function getParentMenuItemId(){
+			return $this->parentMenuItemId;
+		}
+		
+		public function setName($name){
+			$this->name = $name;
+		}
+		
+		public function getName(){
+			return $this->name;
+		}
+		
+		public function setId($id){
+			$this->id = $id;
+		}
+		
+		public function getId($id){
+			return $this->id;
+		}
+		
+		public function assignActionList($actionList){
 			//TODO: Implement
 		}
 		
@@ -237,8 +435,25 @@
 		private $children = array();
 		
 		function __const(){
+			$this->id = null;
+			$this->name = null;
 		}
 		
+		public function setName($name){
+			$this->name = (string)$name;
+		}
+		
+		public function getName(){
+			return $this->name;
+		}
+		
+		public function setId($id){
+			$this->id  = (int)$id;
+		}
+		
+		public function getId($id){
+			return $this->id;
+		}
 		
 		public function addMenuItem($menuItem){
 			//TODO: Class must be MenuItem
