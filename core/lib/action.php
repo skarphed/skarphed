58e935842c40f4b81414da595a62b97800535991
Grindhold <grindhold@gmx.net>
2012-03-22 17:35:39 +0100

    Action-Createfunktionen fertig implementiert

--- core/lib/action.php
+++ core/lib/action.php
@@ -45,20 +45,105 @@
 
 		private $currentParent = null;
 		
-		public function createAction($siteId, $url, $widgetId, $spaceId){
+		public function createAction($actionList=null,$siteId=null, $url=null, $widgetId=null, $spaceId=null){
+			if(!isset($actionList)){
+				return null;
+			}
+			$core = Core::getInstance();
+			$action = new Action();
+			$action->setActionListId($actionList->getId());
+			if (isset($siteId)){
+				$compositeManager = $core->getCompositeManager();
+				$site = $compositeManager->getSite($siteId);
+				if ($site != null){
+					$action->setSiteId($siteId);
+				}else{
+					return null;
+				}
+			}else if (isset($url)){
+				$action->setUrl((string)$url);
+			}else if (isset($widgetId) and isset($spaceId)){
+				$compositeManager = $core->getCompositeManager();
+				$widget = $compositeManager->getWidget($widgetId);
+				if ($widget != null){
+					$action->setWidgetSpaceConstellation($widgetId, $spaceId);
+				}else{
+					return null;
+				}
+			}else{
+				return null;
+			}
+			$action->setName("new action");
+			$db = $core->getDB();
+			$newId = $db->getSeqNext('ACT_GEN');
+			$orderres = $db->query($core,"SELECT MAX(ACT_ORDER) AS MAXORDER FROM ACTIONS WHERE ACT_ATL_ID = ? ;",array($action->getActionListId()));
+			if ($orderset = $db->fetchObject($orderres)){
+				$newOrder = $orderset->MAXORDER;
+			}else{
+				$newOrder = 0;
+			}
+			$action->setId($newId);
+			
+			$db->query($core,"INSERT INTO ACTIONS VALUES (?,?,?,?,?,?,?,?)", 
+						array($action->getId(),$action->getName(), $action->getActionListId(),
+							  $action->getSiteId(), $action->getUrl(), $action->getSpace(),
+							  $action->getWidgetId(), $action->getOrder()));
+			return $action;
+		}
+		
+		public function createActionList($actionListName){
+			$actionList = new ActionList();
+			$actionList->setName($name);
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$actionList->setId($db->getSeqNext('ATL_GEN'));
+			$db->query($core, "INSERT INTO ACTIONLISTS VALUES (?,?);",array($actionList->getId(),$actionList->getName()));
+			return $actionList;
+		}
+		
+		public function createMenuItem($menu=null,$menuItemParent=null, $name="new item"){
+			if (!isset($menu) and !isset($menuItemParent)){
+				return null;
+			}
+			$menuItem = new MenuItem();
+			$menuItem->setName($name);
+			if (isset($menu)){
+				$menuItem->setMenuId($menu->getId());
+				$orderres = $db->query($core, 
+									   "SELECT MAX(MNI_ORDER) AS MAXORDER FROM MENUITEMS WHERE MNI_MNU_ID = ? ;",
+									   array($menu->getId()));
+			}
+			if (isset($menuItemParent)){
+				$menuItem->setParentMenuItemId($menuItemParent->getId());
+				$orderres = $db->query($core, 
+									   "SELECT MAX(MNI_ORDER) AS MAXORDER FROM MENUITEMS WHERE MNI_MNI_ID = ? ;",
+									   array($menuItemParent->getId()));
+			}
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$menuItem->setId($db->getSeqNext('MNI_GEN'));
+			if ($orderset = $db->fetchObject($orderres)){
+				$newOrder = $orderset->MAXORDER;
+			}else{
+				$newOrder = 0;
+			}
+			$menuItem->setOrder($newOrder);
+			$db->query($core ,"INSERT INTO MENUITEMS VALUES (?,?,?,?,?,?) ;",
+						array($menuItem->getId(), $menuItem->getName(),
+						$menuItem->getMenuId(), $menuItem->getParentMenuItemId(),
+						null, $menuItem->getOrder()));
+			return $menuItem;
 			
 		}
 		
-		public function createActionList(){
-			//TODO: Implement
-		}
-		
-		public function createMenuItem(){
-			//TODO: Implement
-		}
-		
-		public function createMenu(){
-			//TODO: Implement
+		public function createMenu($name="new menu"){
+			$core = Core::getInstance();
+			$db = $core->getDB();
+			$menu = new Menu();
+			$menu->setId($db->getSeqNext('MNU_GEN'));
+			$menu->setName($name);
+			$db->query($core,"INSERT INTO MENUS VALUES (?,?) ;",array($menu->getId(), $menu->getName()));
+			return $menu;
 		}
 		
 		public function getActionById($actionId){
@@ -238,6 +323,22 @@
 			$this->spaceId = null;
 			$this->url = null;
 		}
+		
+		public function getWidgetId(){
+			return $this->widgetId;
+		}
+		
+		public function getSpace(){
+			return $this->spaceId;
+		}
+		
+		public function getSiteId(){
+			return $this->siteId;
+		}
+		
+		public function getUrl(){
+			return $this->url;
+		}
 
 	}
 
@@ -260,7 +361,7 @@
 		}
 		
 		public function setName($name){
-			$this->name = $name;
+			$this->name = (string)$name;
 		}
 		
 		public function getName(){
@@ -268,7 +369,7 @@
 		}
 		
 		public function setId($id){
-			$this->id = $id;
+			$this->id = (int)$id;
 		}
 		
 		public function getId(){
@@ -362,6 +463,8 @@
 		
 		public function setMenuId($menuId){
 			$this->menuId = $menuId;
+			$this->parentMenuItem = null;
+			$this->parentMenuItemId = null;
 		}
 		
 		public function getMenu(){
@@ -379,6 +482,8 @@
 		
 		public function setParentMenuItemId($parentMenuItemId){
 			$this->parentMenuItemId = $parentMenuItemId;
+			$this->menu = null;
+			$this->menuId = null;
 		}
 		
 		public function getParentMenuItem(){
@@ -395,7 +500,7 @@
 		}
 		
 		public function setName($name){
-			$this->name = $name;
+			$this->name = (string)$name;
 		}
 		
 		public function getName(){
@@ -403,7 +508,7 @@
 		}
 		
 		public function setId($id){
-			$this->id = $id;
+			$this->id = (int)$id;
 		}
 		
 		public function getId($id){
@@ -437,6 +542,25 @@
 		function __const(){
 			$this->id = null;
 			$this->name = null;
+			$this->siteId = null;
+			$this->site = null;
+		}
+		
+		public function setSiteId($siteId){
+			$this->siteId = (int)$siteId;
+		}
+		
+		public function getSite(){
+			if ($this->site == null or $this->getSiteId() != $this->site->getId()){
+				$core = Core::getInstance();
+				$cm = $core->getCompositeManager();
+				$this->site = $cm->getSite($this->siteId);
+			}
+			return $this->site;
+		}
+		
+		public function getSiteId(){
+			return $this->siteId;
 		}
 		
 		public function setName($name){
@@ -454,6 +578,8 @@
 		public function getId($id){
 			return $this->id;
 		}
+		
+		
 		
 		public function addMenuItem($menuItem){
 			//TODO: Class must be MenuItem
