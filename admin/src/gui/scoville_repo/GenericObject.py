448dba94336d30d465af57f4674d8c5bb5a72fb1
Grindhold <grindhold@gmx.net>
2012-04-17 20:49:00 +0200

    designed a guipage for repository

---
+++ admin/src/gui/scoville_repo/GenericObject.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+###########################################################
+# Copyright 2011 Daniel 'grindhold' Brendle and Team
+#
+# This file is part of Scoville.
+#
+# Scoville is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either 
+# version 3 of the License, or (at your option) any later 
+# version.
+#
+# Scoville is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public 
+# License along with Scoville. 
+# If not, see http://www.gnu.org/licenses/.
+###########################################################
+
+
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+class GenericObjectPage(gtk.VBox):
+    def __init__(self,parent,obj,iconstock=None):
+        gtk.VBox.__init__(self,spacing = 10)
+        self.par = parent
+        
+    def render(self):
+        pass
+    
+    def getPar(self):
+        return self.par
+
+    def getApplication(self):
+        return self.par.getApplication()
+
+class PageFrame(gtk.Frame):
+    def __init__(self,parent, text, icon=None):
+        gtk.Frame.__init__(self)
+        self.set_border_width(10)
+        self.set_label_widget(FrameLabel(self,text,icon))
+        
+        
+
+class FrameLabel(gtk.HBox):
+    def __init__(self,parent, text, icon=None):
+        self.par = parent
+        gtk.HBox.__init__(self)
+        assert type(text) == str, "text must be string"
+        
+        self.icon = gtk.Image()
+        if icon is not None:
+            self.icon.set_from_pixbuf(icon)
+        self.label = gtk.Label()
+        self.label.set_text(text)
+        
+        self.pack_start(self.icon,False)
+        self.pack_start(self.label,True)
+        self.show_all()
+        
+    def getPar(self):
+        return self.par
+
+    def getApplication(self):
+        return self.par.getApplication()
+
+        
+    
+    
+    
---
+++ admin/src/gui/scoville_repo/Scoville_repo.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+###########################################################
+# Copyright 2011 Daniel 'grindhold' Brendle and Team
+#
+# This file is part of Scoville.
+#
+# Scoville is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either 
+# version 3 of the License, or (at your option) any later 
+# version.
+#
+# Scoville is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public 
+# License along with Scoville. 
+# If not, see http://www.gnu.org/licenses/.
+###########################################################
+
+import pygtk
+from gui import IconStock
+pygtk.require("2.0")
+import gtk
+
+from GenericObject import GenericObjectPage
+from GenericObject import PageFrame
+from data.Generic import GenericObjectStoreException
+
+class Scoville_repoPage (GenericObjectPage):
+    def __init__(self,par,repo):
+        self.par = par
+        GenericObjectPage.__init__(self,par,repo)
+        self.repoId = repo.getLocalId()
+        
+        self.table = gtk.Table(2,4,False)
+        
+        
+        
+        
+        self.adminFrame= PageFrame(self, "Repository", IconStock.REPO)
+        self.adminHBox = gtk.HBox()
+        self.adminHBoxDummy = gtk.Label("")
+        self.adminTable= gtk.Table(2,4,False)
+        self.adminLabel = gtk.Label("Change admin-password here:")
+        self.adminPasswordLabel = gtk.Label("New password")
+        self.adminRepeatLabel = gtk.Label("New password (repeat)")
+        self.adminPasswordEntry = gtk.Entry()
+        self.adminPasswordEntry.set_visibility(False)
+        self.adminPasswordEntry.set_invisible_char("●")
+        self.adminRepeatEntry = gtk.Entry()
+        self.adminRepeatEntry.set_visibility(False)
+        self.adminRepeatEntry.set_invisible_char("●")
+        self.adminButtonDummy = gtk.Label()
+        self.adminButtonHBox = gtk.HBox()
+        self.adminButtonChange = gtk.Button(stock=gtk.STOCK_OK)
+        self.adminButtonHBox.pack_start(self.adminButtonDummy,True)
+        self.adminButtonHBox.pack_start(self.adminButtonChange,False)
+        self.adminTable.attach(self.adminLabel,0,2,0,1)
+        self.adminTable.attach(self.adminPasswordLabel,0,1,1,2)
+        self.adminTable.attach(self.adminPasswordEntry,1,2,1,2)
+        self.adminTable.attach(self.adminRepeatLabel,0,1,2,3)
+        self.adminTable.attach(self.adminRepeatEntry,1,2,2,3)
+        self.adminTable.attach(self.adminButtonHBox,0,2,3,4)
+        self.adminHBox.pack_start(self.adminTable,False)
+        self.adminHBox.pack_start(self.adminHBoxDummy,True)
+        self.adminFrame.add(self.adminHBox)
+        self.pack_start(self.adminFrame,False)
+        
+        self.moduleFrame=PageFrame(self, "Modules", IconStock.MODULE)
+        self.moduleList = ModuleList(self,repo)
+        self.moduleFrame.add(self.moduleList)
+        self.pack_start(self.moduleFrame,False)
+        
+        self.developerFrame = PageFrame(self, "Developers", IconStock.USER)
+        self.developerHBox= gtk.HBox()
+        self.developerList= DeveloperList(self,repo)
+        self.developerHBox.pack_start(self.developerList,True)
+        self.developerTable = gtk.Table(2,5,False)
+        self.developerButtonHBox = gtk.HBox()
+        self.developerButtonHBoxDummy = gtk.Label()
+        self.developerLabel = gtk.Label("Please enter the information for a new Developer here:")
+        self.developerNameLabel = gtk.Label("Nickname:")
+        self.developerFullnameLabel = gtk.Label("Full Name:")
+        self.developerPublicKeyLabel = gtk.Label("Public Key:")
+        self.developerNameEntry = gtk.Entry()
+        self.developerFullnameEntry = gtk.Entry()
+        self.developerPublicKeyEntry = gtk.TextView()
+        self.developerAddButton = gtk.Button(stock=gtk.STOCK_ADD)
+        self.developerAddButton.connect("clicked", self.cb_Add)
+        self.developerButtonHBox.pack_start(self.developerButtonHBoxDummy,True)
+        self.developerButtonHBox.pack_start(self.developerAddButton,False)
+        self.developerTable.attach(self.developerLabel,0,2,0,1)
+        self.developerTable.attach(self.developerNameLabel,0,1,1,2)
+        self.developerTable.attach(self.developerNameEntry,1,2,1,2)
+        self.developerTable.attach(self.developerFullnameLabel,0,1,2,3)
+        self.developerTable.attach(self.developerFullnameEntry,1,2,2,3)
+        self.developerTable.attach(self.developerPublicKeyLabel,0,1,3,4)
+        self.developerTable.attach(self.developerPublicKeyEntry,1,2,3,4)
+        self.developerTable.attach(self.developerButtonHBox,0,2,4,5)
+        self.developerHBox.pack_start(self.developerTable,False)
+        self.developerFrame.add(self.developerHBox)
+        self.pack_start(self.developerFrame,False)
+        
+        self.uploadFrame=PageFrame(self, "Upload", IconStock.MODULE)
+        self.uploadbox = gtk.HBox()
+        self.uploadbox.set_border_width(10)
+        self.upload_label = gtk.Label("Please choose the module and click OK")
+        self.upload_filechoose = gtk.FileChooserButton("Select Template", None)
+        self.upload_filechoose.connect("file-set", self.fileChosen)
+        self.upload_filechoose.set_size_request(200,30)
+        self.upload_enter = gtk.Button(stock=gtk.STOCK_OK)
+        self.upload_enter.connect("clicked", self.uploadModule)
+        self.upload_dummy = gtk.Label("")
+        self.uploadbox.pack_start(self.upload_label,False)
+        self.uploadbox.pack_start(self.upload_filechoose,False)
+        self.uploadbox.pack_start(self.upload_enter,False)
+        self.uploadbox.pack_start(self.upload_dummy,True)
+        self.uploadFrame.add(self.uploadbox)
+        self.pack_start(self.uploadFrame,False)
+        
+        self.show_all()
+        
+        repo.addCallback(self.render)
+    
+    def render(self):
+        pass
+    
+    def cb_Add(self,widget=None,data=None):
+        pass
+    
+    def fileChosen(self, widget=None, data=None):
+        self.fileToUpload = widget.get_filename()
+    
+    
+    def uploadModule(self,widget=None,data=None):
+        pass
+    
+    def getPar(self):
+        return self.par
+    
+    def getApplication(self):
+        return self.getPar().getApplication()
+
+class ModuleList(gtk.ScrolledWindow):
+    def __init__(self, par, repo):
+        self.par = par
+        gtk.ScrolledWindow.__init__(self)
+        self.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+        
+        self.treeview = gtk.TreeView()
+        self.store= gtk.ListStore(gtk.gdk.Pixbuf, str)
+        self.treeview.set_model(self.store)
+        self.repoId = repo.getLocalId()
+        
+        self.col = gtk.TreeViewColumn("Module")
+        self.ren_icon = gtk.CellRendererPixbuf()
+        self.ren_text = gtk.CellRendererText()
+        self.col.pack_start(self.ren_icon,False)
+        self.col.pack_start(self.ren_text,True)
+        self.col.add_attribute(self.ren_icon, 'pixbuf',0)
+        self.col.add_attribute(self.ren_text, 'text', 1)
+        self.treeview.append_column(self.col)
+        
+        repo.addCallback(self.render)
+        self.show_all()
+        #self.render()
+        
+    def render(self):
+        repo = self.getApplication().getLocalObjectById(self.repoId)
+        modules = repo.getModules()
+        
+        self.store.clear()
+        for module in modules:
+            self.store.append((IconStock.MODULE,module['hrname']))
+
+    def getPar(self):
+        return self.par
+    
+    def getApplication(self):
+        return self.getPar().getApplication()
+        
+
+class DeveloperList(gtk.ScrolledWindow):
+    def __init__(self, par, repo):
+        self.par = par
+        gtk.ScrolledWindow.__init__(self)
+        self.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+        
+        self.treeview = gtk.TreeView()
+        self.store= gtk.ListStore(gtk.gdk.Pixbuf, str)
+        self.treeview.set_model(self.store)
+        self.repoId = repo.getLocalId()
+        
+        self.col = gtk.TreeViewColumn("Developer")
+        self.ren_icon = gtk.CellRendererPixbuf()
+        self.ren_text = gtk.CellRendererText()
+        self.col.pack_start(self.ren_icon,False)
+        self.col.pack_start(self.ren_text,True)
+        self.col.add_attribute(self.ren_icon, 'pixbuf',0)
+        self.col.add_attribute(self.ren_text, 'text', 1)
+        self.treeview.append_column(self.col)
+        
+        repo.addCallback(self.render)
+        self.show_all()
+        #self.render()
+        
+    def render(self):
+        repo = self.getApplication().getLocalObjectById(self.repoId)
+        developers = repo.getDevelopers()
+        
+        self.store.clear()
+        for developer in developers:
+            self.store.append((IconStock.MODULE,developer['fullName']))
+        
+        
+    def getPar(self):
+        return self.par
+    
+    def getApplication(self):
+        return self.getPar().getApplication()
+        
