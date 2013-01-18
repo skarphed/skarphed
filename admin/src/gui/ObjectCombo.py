e2a3ae973ac4e8ed1cec3e2c8cfacb2e5dee3145
grindhold_laptop <grindhold@localhost>
2013-01-18 14:52:13 +0100

    implemented generict objectType Selectbox

---
+++ admin/src/gui/ObjectCombo.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+###########################################################
+# Copyright 2011 Daniel 'grindhold' Brendle and Team
+#
+# This file is part of Scoville.
+#
+# Scoville is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either 
+# version 3 of the License, or (at your option) any later 
+# version.
+#
+# Scoville is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public 
+# License along with Scoville. 
+# If not, see http://www.gnu.org/licenses/.
+###########################################################
+
+
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+import IconStock
+
+class ObjectCombo(gtk.ComboBox):
+    def __init__(self, par, objectType, selectedObject=None):
+        gtk.ComboBox.__init__(self)
+        self.par = par
+        self.objectType = objectType.__class__.__name__
+        self.selectedObjectId = selectedObject.getLocalId()
+
+        self.model = gtk.ListStore(gtk.gdk.Pixbuf,str,int)
+        
+        self.set_model(self.model)
+        self.iconrenderer = gtk.CellRendererPixbuf()
+        self.pack_start(self.iconrenderer, True)
+        self.add_attribute(self.iconrenderer, "pixbuf", 0)
+        self.namerenderer = gtk.CellRendererText()
+        self.pack_start(self.namerenderer, False)
+        self.add_attribute(self.namerenderer, "text", 1)
+
+        self.getApplication().getObjectStore().addCallback(self.render)
+        self.render()
+
+    def render(self):
+        def search(model, path, rowiter, processed):
+            val = model.get_value(rowiter,2)
+            if val not in processed:
+                model.itersToRemove.append(rowiter)
+        
+        self.obj = self.getApplication().getObjectStore().getAllOfClass(self.objectType)
+        processedObjectIds = []
+        for obj in self.objs:
+            rowiter = self.getIterById(self.model, obj.getLocalId()) 
+            if rowiter is None:
+                self.model.append((IconStock.getServerIcon(obj),obj.getName(),obj.getLocalId()))
+            else:
+                self.model.set_value(rowiter,0,IconStock.getServerIcon(obj))
+                self.model.set_value(rowiter,1,obj.getName())
+            processedObjectIds.append(obj.getLocalId())
+
+
+        if self.selectedObject is not None:
+            activeiter = self.getIterById(self.model, self.selectedObjectId) 
+            self.set_active_iter(activeiter)
+        self.model.itersToRemove = []
+        self.model.foreach(search, processedObjectIds)
+        for rowiter in self.model.itersToRemove:
+            self.model.remove(rowiter)
+
+    def getIterById(self, objlist, objectId):
+        def search(model, path, rowiter, objectId):
+            val = model.get_value(rowiter,2)
+            if val == objectId:
+                model.tempiter = rowiter
+        
+        objlist.tempiter = None
+        objlist.foreach(search, objectId)
+        rowiter=objlist.tempiter
+        if rowiter is not None:
+            return rowiter
+        else:
+            return None
+
+    def getSelected(self):
+        active_iter = self.getActiveIter()
+        objId = self.model.get_value(active_iter,2)
+        obj = self.getApplication().getObjectStore().getLocalObjectById(objId)
+        return obj
+
+    def setSelected(self, obj):
+        if obj.__class__.__name__ != self.objectType:
+            raise TypeError()
+        activeiter = self.getIterById(self.model, obj.getLocalId()) 
+        self.set_active_iter(activeiter)
+        self.selectedObjectId = obj.getLocalId()
+
+    def getPar(self):
+        return self.par
+
+    def getApplication(self):
+        return self.par.getApplication()
