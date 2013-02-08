8ff30d44d3f8427d2b3ddb7fc5b0ef0d097df9eb
grindhold_laptop <grindhold@gmx.net>
2013-02-08 11:10:47 +0100

    documented session.py

--- pycore/lib/session.py
+++ pycore/lib/session.py
@@ -38,6 +38,9 @@ class SessionException(Exception):
         return "SE_"+str(nr)+": "+cls.ERRORS[nr]+" "+info
 
 class SessionManager(object):
+    """
+    Wraps Session's classmethod
+    """
     def __init__(self, core):
         self._core = core
         Session.set_core(core)
@@ -49,14 +52,25 @@ class SessionManager(object):
         self.get_current_session_user = Session.get_current_session_user
 
 class Session(object):
+    """
+    Handles clientsessions by a given session_id
+    TODO: Session saves environmental variablees
+    TODO: Session takes env-vars in account when verifying the id of the client
+    """
     ACTIVE_SESSIONS = {}
     CURRENT_SESSION = None
     @classmethod
     def set_core(cls,core):
+        """
+        trivial
+        """
         cls._core = core
 
     @classmethod
     def get_session(cls,session_id):
+        """
+        returns the session if it's not expired or nonexistant
+        """
         if not cls.ACTIVE_SESSIONS.has_key(session_id):
             raise SessionException(SessionException.get_msg(2))
         session = cls.ACTIVE_SESSIONS[session_id]
@@ -68,21 +82,37 @@ class Session(object):
 
     @classmethod
     def create_session(cls, user):
+        """
+        creates a session for a given user
+        """
         return Session(cls._core,user)
 
     @classmethod
     def set_current_session(cls, session):
+        """
+        sets the session of the current user
+        """
         cls.CURRENT_SESSION = session
 
     @classmethod
     def get_current_session(cls):
+        """
+        returns the current session of the current client
+        """
         return cls.CURRENT_SESSION
 
     @classmethod
     def get_current_session_user(cls):
+        """
+        returns the user of the current clientsession
+        """
         return cls.CURRENT_SESSION.get_user()
 
     def _generateRandomString(self,length):
+        """
+        generate  a random string 
+        TODO: outsource this function to some kind of helpers module (other occurence in database.py)
+        """
         ret = ""
         for i in range(length):
             x = -1
@@ -92,6 +122,9 @@ class Session(object):
         return ret
 
     def __init__(self,core, user):
+        """
+        initializes a session
+        """
         self._core = core
         configuration = self._core.get_configuration() #TODO: Introduce Configvalue sessionduration
 
@@ -100,15 +133,24 @@ class Session(object):
         self._expiration = datetime.now()+timedelta(0,int(configuration.get_entry("core.session_duration"))*3600)
         Session.ACTIVE_SESSIONS[self._id] = self
 
-    def touch(self):
+    def extend(self):
+        """
+        extends this session by a timespan defined as "core.session_extend" in hours in configuration
+        """
         configuration = self._core.get_configuration()
         self._expiration += timedelta(0,int(configuration.get_entry("core.session_extend"))*3600)
 
     def get_user(self):
+        """
+        returns this session's user
+        """
         usermanager = self._core.get_user_manager()
         return usermanager.get_user(self._user)
 
     def set_user(self,user):
+        """
+        set the user of this session
+        """
         if type(user) == int:
             self._user = user
         elif user.__class__.__name__ == "User":
