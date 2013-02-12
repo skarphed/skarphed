573657fd0c1c1f29b265dd9b1fc7c89abfcee353
grindhold_laptop <grindhold@gmx.net>
2013-02-12 23:50:24 +0100

    began implementing css.py

--- pycore/lib/css.py
+++ pycore/lib/css.py
@@ -59,9 +59,119 @@ import re
 
 class CSSException(Exception):
     ERRORS = {
-        1:"""Only the Configuration-class is authorized to access this variable"""
+        0:"""Get CSSPropertySet: This Widget does not exist"""
     }
 
     @classmethod
     def get_msg(cls,nr, info=""):
         return "CSS_"+str(nr)+": "+cls.ERRORS[nr]+" "+info
+
+class CSSManager(object):
+    pass
+
+class CSSPropertySet(object):
+    ALL = -1
+    @classmethod
+    def create_csspropertyset_from_serial(cls, serial):
+        """
+        creates a propertyset from a serialized dataformat
+        """
+        css_property_set = CSSPropertySet()
+        css_property_set.build_serialized(serial)
+        return css_property_set
+
+    @classmethod
+    def get_csspropertyset(cls, module_id=None,widget_id=None,session_id=None,with_inherited=True):
+        db = cls._core.get_db()
+        if module_id is not None:
+            if module_id != CSSPropertySet.ALL:
+                ret = {}
+                stmnt = "SELECT CSS_SELECTOR, CSS_TAG, CSS_VALUE, MOD_NAME, MOD_ID \
+                                 FROM CSS \
+                                   INNER JOIN MODULES ON (CSS_MOD_ID = MOD_ID) \
+                                 WHERE CSS_MOD_ID IS NOT NULL AND CSS_WGT_ID IS NULL AND CSS_SESSION IS NULL ;"
+                cur = db.query(cls._core,stmnt)
+                rows = cur.fetchmapall()
+                for row in rows:
+                    if not ret.has_key(row["MOD_ID"]):
+                        ret[row["MOD_ID"]] = CSSPropertySet()
+                        ret[row["MOD_ID"]].set_module_id(row["MOD_ID"])
+                    ret[row["MOD_ID"]].edit_value(row["CSS_SELECTOR"], row["CSS_TAG"], row["CSS_VALUE"])
+                return ret
+            else:
+                if with_inherited:
+                    propertyset = cls.get_csspropertyset()
+                    propertyset.set_all_inherited()
+                else:
+                    propertyset = CSSPropertySet()
+                propertyset.set_module_id(module_id)
+                stmnt = "SELECT CSS_SELECTOR, CSS_TAG, CSS_VALUE, MOD_NAME \
+                             FROM CSS \
+                               INNER JOIN MODULES ON (CSS_MOD_ID = MOD_ID) \
+                             WHERE CSS_MOD_ID IS NOT NULL AND CSS_WGT_ID IS NULL AND CSS_SESSION IS NULL AND CSS_MOD_ID = ? ;";
+                cur = db.query(cls._core,stmnt,(module_id,))
+                rows = cur.fetchmapall()
+                for row in rows:
+                    propertyset.edit_value(row["CSS_SELECTOR"],row["CSS_TAG"], row["CSS_VALUE"])
+                return propertyset
+        if widget_id is not None:
+            if widget_id == CSSPropertySet.ALL:
+                ret = {}
+                stmnt = "SELECT CSS_SELECTOR, CSS_TAG, CSS_VALUE, MOD_NAME, MOD_ID, WGT_NAME, WGT_ID \
+                         FROM CSS \
+                           INNER JOIN WIDGETS ON (CSS_WGT_ID = WGT_ID) \
+                           INNER JOIN MODULES ON (WGT_MOD_ID = MOD_ID) \
+                         WHERE CSS_MOD_ID IS NULL AND CSS_WGT_ID IS NOT NULL AND CSS_SESSION IS NULL ;"
+                cur = db.query(cls._core,stmnt)
+                rows = cur.fetchmapall()
+                for row in rows:
+                    if not ret.has_key(row["WGT_ID"]):
+                        ret[row["WGT_ID"]] = CSSPropertySet()
+                        ret[row["WGT_ID"]].set_widget_id(row["WGT_ID"])
+                    ret[row["WGT_ID"]].edit_value(row["CSS_SELECTOR"],row["CSS_TAG"],row["CSS_VALUE"])
+                return ret
+            else:
+                if with_inherited:
+                    stmnt = "SELECT WGT_MOD_ID FROM WIDGETS WHERE WGT_ID = ? ; "
+                    cur = db.query(cls._core,stmnt,(widget_id,))
+                    row = cur.fetchone()
+                    if row:
+                        propertyset = cls.get_csspropertyset(module_id = row[0])
+                        propertyset.set_all_inherited()
+                    else:
+                        raise CSSException(CSSException.get_msg(0))
+                else:
+                    propertyset = CSSPropertySet()
+                propertyset.set_widget_id(widget_id)
+                stmnt = "SELECT CSS_SELECTOR, CSS_TAG, CSS_VALUE, MOD_NAME, WGT_NAME \
+                             FROM CSS \
+                               INNER JOIN WIDGETS ON (CSS_WGT_ID = WGT_ID) \
+                               INNER JOIN MODULES ON (WGT_MOD_ID = MOD_ID) \
+                             WHERE CSS_MOD_ID IS NULL AND CSS_WGT_ID IS NOT NULL AND CSS_SESSION IS NULL AND CSS_WGT_ID = ? ;"
+                cur = db.query(cls._core,stmnt,(widget_id,))
+                rows = cur.fetchmapall()
+                for row in rows:
+                    propertyset.edit_value(row["CSS_SELECTOR"], row["CSS_TAG"], row["CSS_VALUE"])
+                return propertyset
+        if session_id is not None:
+            return None
+            #TODO: Implement
+
+        #Standard CSS Propertyset
+        propertyset = CSSPropertySet()
+        propertyset.set_type_general()
+        stmnt = "SELECT CSS_SELECTOR, CSS_TAG, CSS_VALUE FROM CSS WHERE CSS_MOD_ID IS NULL AND CSS_WGT_ID IS NULL AND CSS_SESSION IS NULL ;"
+        cur = db.query(cls._core,stmnt)
+        rows = cur.fetchmapall()
+        for row in rows:
+            propertyset.edit_value(row["CSS_SELECTOR"], row["CSS_TAG"], row["CSS_VALUE"])
+        return propertyset
+
+    @classmethod
+    def render(filename):
+        pass
+
+
+
+class CSSParser(iter): #TODO find proper iter handling method in python (yield and stuff)
+    pass
