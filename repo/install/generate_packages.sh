174176829358b358782bd73a32534271412eb39e
grindhold <grindhold@gmx.net>
2013-04-18 09:59:40 +0200

    changed shebang to #!/bin/bash instead of #!/bin/sh doesnt work otherwise on deb squeeze

--- repo/install/generate_packages.sh
+++ repo/install/generate_packages.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 deb_root="./deb/scoville-repo/"
 
-function generate_deb {	
+function generate_deb {
 	cp -r ../src/* ${deb_root}var/www/scvrepo/
 	cp ../gen_keypair.py ../repo_database.sql ${deb_root}tmp/
 	cp ../config.json ${deb_root}etc/scvrepo/
 	cp ../scvrepo_apache2 ${deb_root}etc/apache2/sites-enabled/scvrepo
 	cp -r ../static ${deb_root}usr/share/scvrepo/
 	cp ../templates/template.html ${deb_root}usr/share/scvrepo/
-	dpkg-deb -z6 -Zgzip --build ${deb_root} 
+	dpkg-deb -z6 -Zgzip --build ${deb_root}
 	mv "./deb/scoville-repo.deb" .
 }
 
