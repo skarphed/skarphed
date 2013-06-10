28b1605ca0f4ad8189e5ef1349bdd3bff14f9a68
grindhold <grindhold@gmx.net>
2013-06-10 05:01:02 +0200

    added py2exe buildscript and installer buildscript for windows

---
+++ admin/src/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+import py2exe
+import os
+import sys
+import shutil
+
+# Find GTK+ installation path
+__import__('gtk')
+m = sys.modules['gtk']
+gtk_base_path = m.__path__[0]
+
+try:
+	shutil.rmtree("dist")
+except:
+	pass
+
+try:
+	shutil.rmtree("build")
+except:
+	pass
+
+shutil.copytree("..\data","dist\\data")
+shutil.copytree("..\installer","dist\\installer")
+shutil.copytree("..\locale","dist\\locale")
+
+setup(
+	name='scoville-admin',
+	description='Scoville Administration Interface',
+	version='0.1alpha',
+	windows= [
+	    {
+	    	'script':'Application.py',
+	    	'icon_resources':[(1,'scoville.ico')]
+	    }
+	],
+	options = {
+		'py2exe': {
+			'packages':'encodings',
+			'includes': 'cairo, pango, pangocairo, atk, gobject, gio, gtk.keysyms'
+		}
+	},
+	
+)
+
