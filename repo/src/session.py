a1cae9a478ed692af84313136f7fb6f583ac0b92
grindhold <grindhold@gmx.net>
2013-06-24 02:19:31 +0200

    AGPLed the project

--- repo/src/session.py
+++ repo/src/session.py
@@ -1,10 +1,10 @@
 ###########################################################
-# Copyright 2011 Daniel 'grindhold' Brendle and Team
+# © 2011 Daniel 'grindhold' Brendle and Team
 #
 # This file is part of Skarphed.
 #
 # Skarphed is free software: you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License 
+# modify it under the terms of the GNU Affero General Public License 
 # as published by the Free Software Foundation, either 
 # version 3 of the License, or (at your option) any later 
 # version.
@@ -12,9 +12,9 @@
 # Skarphed is distributed in the hope that it will be 
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
-# PURPOSE. See the GNU General Public License for more details.
+# PURPOSE. See the GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public 
+# You should have received a copy of the GNU Affero General Public 
 # License along with Skarphed. 
 # If not, see http://www.gnu.org/licenses/.
 ###########################################################
