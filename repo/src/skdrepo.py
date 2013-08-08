b04e27167ac245518cb651974e3719ecb43c3458
Andre Kupka <kupka@in.tum.de>
2013-08-08 19:06:15 +0200

    standalone repository is now working with a wsgiref simple_server

--- repo/src/skdrepo.py
+++ repo/src/skdrepo.py
@@ -25,49 +25,11 @@
 
 import sys
 sys.path.append('/usr/share/skdrepo/')
-sys.path.append('/var/www/skdrepo/')
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from wsgiref.simple_server import make_server
 
 from config import Config
 import wsgi
-
-
-class RepositoryHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.handle_request(self.command)
-
-    def do_POST(self):
-        self.handle_request(self.command)
-
-    def handle_request(self, method):
-        def start_response(status, headers, exc_info=None):
-            status = int(status.split(' ')[0])
-            self.send_response(status)
-            for (key, value) in headers:
-                self.send_header(key, value)
-            self.end_headers()
-
-        environ = self.create_wsgi_environ()
-        response_body = wsgi.application(environ, start_response)
-        self.wfile.write(response_body[0])
-
-    def create_wsgi_environ(self):
-
-        print(self.path)
-        print(self.command)
-        print(self.headers)
-
-        environ = {} 
-        query_string = self.path.split('?', 1)
-        if len(query_string) == 2:
-            query_string = query_string[1]
-        else:
-            query_string = ''
-        environ['QUERY_STRING'] = query_string
-        environ['REQUEST_METHOD'] = self.command
-        environ['PATH_INFO'] = self.path
-        return environ
 
 
 def main():
@@ -75,9 +37,8 @@ def main():
     Executes a skprepo as standalone application. 
     """
     config = Config()
-    listen_address = (config['server.ip'], config['server.port'])
 
-    httpd = HTTPServer(listen_address, RepositoryHTTPRequestHandler)
+    httpd = make_server(config['server.ip'], config['server.port'], wsgi.application)
     httpd.serve_forever()
     
     
