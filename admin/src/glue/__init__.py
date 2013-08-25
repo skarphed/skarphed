039b246b84182723991eb87b1e9eafefc0c669e8
grindhold <grindhold@gmx.net>
2013-08-25 14:30:44 +0200

    realized git submodules in repo with own history

--- admin/src/glue/__init__.py
+++
@@ -1,4 +0,0 @@
-############################################################
-# This folder contains some helper-functions and classes   #
-# that will be used from time to time throughout skarphed  #
-############################################################
