e00f57f9af07ecb3fae77641d221ec35ccfa673a
grindhold <grindhold@gmx.net>
2013-08-26 21:19:21 +0200

    Added basic unittests for users.py

--- testing/skarphed-core/workspace/tests/users.py
+++ testing/skarphed-core/workspace/tests/users.py
@@ -23,13 +23,56 @@
 ###########################################################
 
 from skd_test import CoreTestCase
+from common.errors import UserException
 
 class TestUserFunctions(CoreTestCase):
-	def setUp(self):
-		CoreTestCase.setUp(self)
+    def setUp(self):
+        CoreTestCase.setUp(self)
+        user_manager = self._core.get_user()
+        self.testuser = user_manager.create_user("testuser","testpassword")
 
-	def test_blubb(self):
-		self.assertEqual(0,0)
+    def test_create_user(self):
+        user_manager = self._core.get_user_manager()
+        user = user_manager.create_user("testuser2","testpassword")
+        self.assertEqual(user.get_name(),"testuser2")
 
-	def tearDown(self):
-		CoreTestCase.tearDown(self)
+        stored_user = user_manager.get_user_by_name("testuser2")
+        self.assertEqual(user_manager.get_user_by_name("testuser2").get_id(),user.get_id())
+
+    def test_get_user_by_name(self):
+        user_manager = self._core.get_user_manager()
+        self.assertEqual(user_manager.get_user_by_name("testuser").get_id(),self.testuser.get_id())
+
+    def test_get_user_by_id(self):
+        nr = self.testuser.get_id()
+        user_manager = self._core.get_user_manager()
+        self.assertEqual(user_manager.get_user_by_id(nr).get_id(),self.testuser.get_id())        
+
+    def test_authenticate(self):
+        self.assertTrue(self.testuser.authenticate("testpassword"))
+
+    def test_authenticate_false(self):
+        try:
+            self.testuser.authenticate("falsepassword")
+        except UserException, e:
+            pass
+        else:
+            self.assertFalse(True)
+
+    def test_alter_password(self):
+        self.testuser.alter_password("newtestpassword","testpassword")
+        self.assertTrue(self.testuser.authenticate("newtestpassword"))
+        self.testuser.alter_password("testpassword","newtestpassword")
+        self.assertTrue(self.testuser.authenticate("testpassword"))
+
+    def test_alter_password_false(self):
+        try:
+            self.testuser.alter_password("newtestpassword","falsepassword")
+        except UserException, e:
+            pass
+        else:
+            self.assertFalse(True)
+
+    def tearDown(self):
+        CoreTestCase.tearDown(self)
+        self.testuser.delete()
